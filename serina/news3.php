734e1839bfc7a87ed1479ee5af52f8b15f39d5a3
sp35000 <sp35000@yahoo.com.br>
2023-03-01 18:05:12 -0300

    feat: Serina: news2/3.php share buttons added

--- serina/news3.php
+++ serina/news3.php
@@ -76,5 +76,8 @@ $conn->close();
 </div>
 </div>
 <script>includeHTML();</script>
+<!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script type="text/javascript"
+src="https://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5e8671c07f6deb95"></script>
  </body>
 </html>
