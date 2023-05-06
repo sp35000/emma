2ea9ecb7d5e75c804b68357d5bf9a93518400809
sp35000 <sp35000@yahoo.com.br>
2023-05-06 16:32:12 -0300

    feat: updating links

--- serina/get_title_python.php
+++ serina/get_title_python.php
@@ -1,4 +1,5 @@
 <?php
+// echo get_include_path();
 $url = $_REQUEST["url"];
 $title = system("/home/yzmu/git/side_projects/getTitle.py ".$url,$retCode);
 #echo "/home/yzmu/pythonStudy/getTitle.py ".$url." >2&1";
